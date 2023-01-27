38cd9316d133ee1c30321bb4357760ba739577fb
Donghyeon Lee <ehdgus001928@gmail.com>
2023-01-27 19:36:37 +0900

    Update UI style

--- src/components/matchList.tsx
+++ src/components/matchList.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 
 export interface IMatchData {
@@ -6,9 +8,7 @@ export interface IMatchData {
 }
 
 export interface IParticipantsInfoData {
-  summonerId: string;
   summonerName: string;
-  puuid: string;
   teamId: number;
   win: boolean;
   kills: number;
@@ -20,15 +20,11 @@ export interface IParticipantsInfoData {
   goldEarned: number;
   totalDamageDealtToChampions: number;
   totalDamageTaken: number;
+  totalMinionsKilled: number;
   visionScore: number;
   sightWardsBoughtInGame: number;
-  item0: number;
-  item1: number;
-  item2: number;
-  item3: number;
-  item4: number;
-  item5: number;
-  item6: number;
+  items: number[];
+  summonerSpellIds: number[];
 }
 
 export interface IReducedMatchData {
@@ -46,41 +42,165 @@ interface IMatchListProps {
   data: IReducedMatchData[];
 }
 
+const MatchListContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+`
+
+const MatchListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  margin: 1rem;
+  list-style: none;
+  /* border-radius: 12px; */
+`
+
+const MatchResultColorBox = styled.div<{ win: boolean }>`
+  width: 16px;
+  margin: 0.4rem;
+  border-radius: 12px;
+  background-color: ${props => props.win ? '#0000ff64;' : '#ff00006a;'};
+`
+
+const MatchInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  align-self: center;
+`
+
+const SummonerInfoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  align-self: center;
+`
+
+const SummonerSpellInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const SummonerItemBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+`
+
+const DetailBox = styled.div`
+  align-self: center;
+  padding: 20px;
+`
+
+const BlueTeamList = styled.ul`
+  margin: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  /* background-color: #0000ff8c; */
+`
+
+const RedTeamList = styled.ul`
+  margin: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  /* background-color: #ff00009c; */
+`
+
+const Foo = styled.div`
+  flex: 1;
+`
+
 export default function MatchList(props: IMatchListProps) {
 
   return (
-    <ul>
-      {props.data.map((match: IReducedMatchData) => (
-        <li key={match.matchId}>
-            <p>matchId: {match.matchId}</p>
-            {/* <p>{match.gameEndTimestamp}</p> */}
-            <p>currentSummoner: {match.participantsInfo[match.currentSummonerIndex].championName}</p>
-            <p>{match.participantsInfo[match.currentSummonerIndex].win ? "win" : "lose"}</p>
-            <p>gameDuration: {match.gameDuration}</p>
-            <p>participants</p>
-            <p>#blue Team</p>
-            {match.participantsInfo.map((pData: IParticipantsInfoData) => {
-              if (pData.teamId === 100) {
+    <MatchListContainer>
+      {props.data.map((match: IReducedMatchData) => {
 
-                return (
-                  <p key={pData.summonerId}>{pData.summonerName}</p>
-                );
-              }
-            })}
-            <p>#red Team</p>
-            {match.participantsInfo.map((pData: IParticipantsInfoData) => {
-              if (pData.teamId === 200) {
+        const player: IParticipantsInfoData = match.participantsInfo[match.currentSummonerIndex];
 
-                return (
-                  <p key={pData.summonerId}>{pData.summonerName}</p>
-                );
-              }
-            })}
-            {/* <p>{match.gameMode}</p>
-            <p>{match.gameType}</p> */}
+        return (
+          <MatchListItem key={match.matchId}>
+            <MatchResultColorBox
+              win={player.win} />
+            <MatchInfo>
+              <p>matchId: {match.matchId}</p>
+              <p>{match.gameMode}</p>
+              <p>{player.win ? "win" : "loss"}</p>
+              <p>{match.gameDuration}</p>
+            </MatchInfo>
+            <SummonerInfoBox>
+              <Image 
+                src={`/image/champion/${player.championName}.png`} 
+                alt="err" 
+                width={50} 
+                height={50}
+              />
+              <SummonerSpellInfoBox>
+                {player.summonerSpellIds.map((id: number) => (
+                  <Image 
+                    key={id}
+                    src={`/image/summonerSpell/${id}.png`} 
+                    alt="err" 
+                    width={24} 
+                    height={24}
+                    style={{ margin: 2, marginBottom: 1 }}
+                  />
+                ))}
+              </SummonerSpellInfoBox>
+            </SummonerInfoBox>
+            <SummonerItemBox>
+              {player.items.map((id: number) => (
+                <Image 
+                  key={id}
+                  src={`/image/item/${id}.png`} 
+                  alt="err" 
+                  width={40} 
+                  height={40}
+                  style={{ margin: 2 }}
+                />
+              ))}
+            </SummonerItemBox>
+            <DetailBox>
+              K/D/A: {player.kills}&nbsp;/&nbsp;{player.deaths}&nbsp;/&nbsp;{player.assists}
+              <br />totalDamageDealt: {player.totalDamageDealtToChampions}
+              <br />totalDamageTaken: {player.totalDamageTaken}
+              <br />cs: {player.totalMinionsKilled}
+            </DetailBox>
+            <Foo />
+            <BlueTeamList>
+              <p>#blue Team</p>
+              {match.participantsInfo.map((pData: IParticipantsInfoData) => {
+                if (pData.teamId === 100) {
+
+                  return (
+                    <Link 
+                      href={`/search/${pData.summonerName}`} 
+                      key={pData.summonerName}>
+                        {pData.summonerName}
+                    </Link>
+                  );
+                }
+              })}
+            </BlueTeamList>
+            <RedTeamList>
+              <p>#red Team</p>
+              {match.participantsInfo.map((pData: IParticipantsInfoData) => {
+                if (pData.teamId === 200) {
+
+                  return (
+                    <Link 
+                      href={`/search/${pData.summonerName}`} 
+                      key={pData.summonerName}>
+                        {pData.summonerName}
+                    </Link>
+                  );
+                }
+              })}
+            </RedTeamList>
             <br />
-        </li>
-      ))}
-    </ul>
+          </MatchListItem>)}
+      )}
+    </MatchListContainer>
   );
 }
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 
 export interface IRankData {
   leagueId: string;
@@ -24,22 +25,36 @@ interface IProfileProps {
   data: IProfileData;
 }
 
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem;
+`
+
+const ProfileItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`
+
 export default function Profile(props: IProfileProps) {
   
   return (
-    <div>
-      <p>summonerName: {props.data.name}</p>
-      <p>summonerLevel: {props.data.summonerLevel}</p>
-      <p>profileIconId: {props.data.profileIconId}</p>
-      <br />
+    <ProfileContainer>
+      <ProfileItem>
+        <p>summonerName: {props.data.name}</p>
+        <p>summonerLevel: {props.data.summonerLevel}</p>
+        <p>profileIconId: {props.data.profileIconId}</p>
+      </ProfileItem>
       {props.data.rank.map((rankData: IRankData) => (
-        <>
+        <ProfileItem key={rankData.queueType}>
           <p>{rankData.queueType}</p>
-          <p>{rankData.tier}&nbsp;{rankData.rank}&nbsp;{rankData.leaguePoints}&nbsp;points</p>
+          <p>{rankData.tier}&nbsp;{rankData.rank}</p>
+          <p>{rankData.leaguePoints}&nbsp;points</p>
           <p>{rankData.wins}&nbsp;wins&nbsp;&nbsp;{rankData.losses}&nbsp;losses</p>
           <br />
-        </>
+        </ProfileItem>
       ))}
-    </div>
+    </ProfileContainer>
   )
 }
